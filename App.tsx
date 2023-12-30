373005d2721f7dcce680725d4fcd872f9c867872
PabloViniciusSS <48532322+PabloViniciusSS@users.noreply.github.com>
2023-12-30 10:42:10 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,81 @@
+import { useEffect, useRef } from 'react';
+import { View } from 'react-native';
+import { requestForegroundPermissionsAsync, 
+  getCurrentPositionAsync, 
+  LocationObject,
+  watchPositionAsync,
+  LocationAccuracy
+} from 'expo-location';
+import { useState } from 'react';
+import MapView, {Marker} from 'react-native-maps';
+
+
+import styles from './style';
+
+export default function App() {
+
+  const [location, setLocation] = useState<LocationObject | null>(null)
+    
+  const mapRef = useRef<MapView>(null)
+
+  async function requestForegroundPermissions(){
+      const { granted } = await requestForegroundPermissionsAsync()
+  
+      if(granted) {
+        const currentPosition = await getCurrentPositionAsync()
+     
+     
+      }
+
+    }
+
+
+useEffect(() => {
+  requestForegroundPermissions()
+
+}, [])
+
+
+useEffect(() => {
+  watchPositionAsync({
+    accuracy: LocationAccuracy.Highest,
+    timeInterval: 1000,
+    distanceInterval: 1
+  }, (response) => {
+    setLocation(response)
+    mapRef.current?.animateCamera({
+      pitch: 70,
+      center: response.coords
+    })
+
+  })
+
+}, [])
+
+  return (
+    <View style={styles.container}>
+
+      {
+        location &&
+      <MapView
+      ref={mapRef}
+      style={styles.map}
+      initialRegion={{
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0005,
+        longitudeDelta: 0.0005
+        
+      }}
+      >
+        <Marker
+        coordinate={{
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+        }}
+        />
+      </MapView>
+}
+    </View>
+  );
+}
